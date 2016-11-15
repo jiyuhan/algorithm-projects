5832d06554a3ad6546dd590915b534e0cd4bfc5e
jiyu han <jiyu-han@uiowa.edu>
2016-11-15 01:43:40 -0600

    project 3 not done

--- Algorithm_Project3/src/Maze.java
+++ Algorithm_Project3/src/Maze.java
@@ -1,12 +1,47 @@
 import java.util.*;
 
-public class Maze extends UnionFind{
+public class Maze {
 
     private static final int right = 0;
     private static final int down = 1;
     private static final int left = 2;
     private static final int up = 3;
     private static Random randomGenerator;  // for random numbers
+
+    int[] parent;
+    int[] depth;
+
+    // validate that p is a valid index
+    private void validate(int p) {
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n-1));
+        }
+    }
+
+    public int find(int p) {
+        validate(p);
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];    // path compression by halving
+            p = parent[p];
+        }
+        return p;
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+
+        // make root of smaller rank point to root of larger rank
+        if      (depth[rootP] < depth[rootQ]) parent[rootP] = rootQ;
+        else if (depth[rootP] > depth[rootQ]) parent[rootQ] = rootP;
+        else {
+            parent[rootQ] = rootP;
+            depth[rootP]++;
+        }
+    }
+
 
     private static int size;
 
@@ -19,17 +54,6 @@ public class Maze extends UnionFind{
     private static Edge[][] graph;
     private static int N;   // number of points in the graph
 
-    /**
-     * Initializes an empty union–find data structure with {@code n} sites
-     * {@code 0} through {@code n-1}. Each site is initially in its own
-     * component.
-     *
-     * @param n the number of sites
-     * @throws IllegalArgumentException if {@code n < 0}
-     */
-    public Maze(int n) {
-        super(n);
-    }
 
     /**
      * this is a class that describes one point's location in a maze with x and y
@@ -68,6 +92,7 @@ public class Maze extends UnionFind{
         boolean used;     // for maze creation
         boolean deleted;  // for maze creation
 
+
         // Constructor with a point (location) and direction
         private Edge(Point p, int d) {
             this.point = p;
@@ -75,6 +100,8 @@ public class Maze extends UnionFind{
             this.used = false;
             this.deleted = false;
         }
+
+
     }
 
     private static void setupEmptyMaze() {
@@ -85,25 +112,27 @@ public class Maze extends UnionFind{
 
         // Create board and graph.
         board = new Point[size][size];
-        N = size*size;  // number of points
+        N = size * size;  // number of points
         graph = new Edge[N][4];
 
         /*
          *  This part generates and connects the edges
          */
-        for (int i = 0; i < size; ++i)
+        for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
+
                 Point p = new Point(i, j);
+                System.out.format("%d %d%n", p.getX(), p.getY());
                 int pindex = i*size+j;   // Point(i, j)'s index is i*Size + j
-
                 board[i][j] = p;
-
-                graph[pindex][right] = (j < size-1)? new Edge(p, right): dummy;
-                graph[pindex][down] = (i < size-1)? new Edge(p, down) : dummy;
-                graph[pindex][left] = (j > 0)? graph[pindex-1][right] : dummy;
-                graph[pindex][up] = (i > 0)? graph[pindex-size][down] : dummy;
-
+                System.out.format("%d %d%n", board[i][j].getX(), board[i][j].getY());
+                /* this creates and connects the points in the maze to graph*/
+                graph[pindex][right] = (j < size - 1) ? new Edge(p, right) : dummy;
+                graph[pindex][down] = (i < size - 1) ? new Edge(p, down) : dummy;
+                graph[pindex][left] = (j > 0) ? graph[pindex - 1][right] : dummy;
+                graph[pindex][up] = (i > 0) ? graph[pindex - size][down] : dummy;
             }
+        }
     }
 
 
@@ -131,12 +160,31 @@ public class Maze extends UnionFind{
         System.out.println();
     }
 
+    /**
+     * this function prints how the maze looks like after deleting edges
+     */
     private static void printMaze() {
-        System.out.println("\nPrint the generated maze...");
+        System.out.println("\nPrinting the generated maze...");
+
+        /*for(int i = 0; i < size; i++) {
+            System.out.print("\t-");
+            for(int j = 0; j < size; j++) {
+                if(graph[i][j] == ) System.out.print("----");
+
+            System.out.println();
+        }*/
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < 4; j++) {
+                System.out.format("(%d, %d), %d\t\t", graph[i][j].point.getX(), graph[i][j].point.getY(), graph[i][j].direction);
+            }
+            System.out.println();
+        }
 
         for(int i = 0; i < size; i++) {
-            System.out.print("\t-");
-            for(int j = 0; j < size; j++) System.out.print("----");
+            for(int j = 0; j < size; j++) {
+                System.out.format("(%d, %d)%n", board[i][j].getX(), board[i][j].getY());
+            }
             System.out.println();
         }
     }
@@ -154,16 +202,23 @@ public class Maze extends UnionFind{
         scan.close();
     }
 
+
+
     private static int getARandomNumber() {
         // Hint: To randomly pick an edge in the maze, you may
         // randomly pick a point first, then randomly pick
         // a direction to get the edge associated with the point.
         randomGenerator = new Random();
-        return randomGenerator.nextInt(N);
+        return randomGenerator.nextInt(N - 1);
     }
 
-    private static int createMaze() {
-
+    private int createMaze() {
+        while(find(0) != find(size * size - 1)) {
+            while(true) {
+                int rand = getARandomNumber();
+            }
+        }
+        return 0;
     }
 
     /**
@@ -175,7 +230,7 @@ public class Maze extends UnionFind{
         getMazeSize();
         setupEmptyMaze();
         displayInitBoard();
-
+        printMaze();
         int i = getARandomNumber();
         System.out.println("\nA random number between 0 and " + (N-1) + ": " + i);
 
